30d5203b3cb5f1057f974bb081c485675ce392a5
uhorn <uwehornnet@users.noreply.github.com>
2023-12-20 21:00:43 +0100

    first commit

--- pages/api/google/ads/get.js
+++ pages/api/google/ads/get.js
@@ -3,38 +3,27 @@ const path = require("path");
 
 import OpenAI from "openai";
 
+import createCSV from "../../../../utils/createCSV";
+
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 });
 
 const fetchAPIResponse = async ({ keyword, length }) => {
+	const prompt = `write 3, komma separated, google ads textlines (max ${length} letters) with high click through rates from keyword: "${keyword}"`;
+
 	const response = await openai.chat.completions.create({
 		messages: [
 			{
 				role: "system",
-				content: `write 3, komma separated, google ads textlines with high click through rates and a maximum length of letters of ${length} from keyword: ${keyword}`,
+				content: prompt,
 			},
 		],
 		model: "gpt-3.5-turbo",
 		temperature: 0.3,
 	});
+
 	return response.choices[0].message;
-};
-
-const createCSV = (data) => {
-	const csvRows = [];
-	const headers = Object.keys(data[0]);
-	csvRows.push(headers.join(","));
-
-	for (const row of data) {
-		const values = headers.map((header) => {
-			const escaped = ("" + row[header]).replace(/"/g, '\\"');
-			return `"${escaped}"`;
-		});
-		csvRows.push(values.join(","));
-	}
-
-	return csvRows.join("\n");
 };
 
 export default async function handler(req, res) {
@@ -42,37 +31,36 @@ export default async function handler(req, res) {
 	const filename = "google-ads.csv";
 	const file = path.join(process.cwd(), "public/files", filename);
 
-	//const keywords = params.split(",");
+	const keywords = params.split(",");
+	const apiResponse = await Promise.all(
+		keywords.map(async (keyword) => {
+			const apiResponse = await fetchAPIResponse({ keyword, length });
+			const apiResponseArray = apiResponse.content.split("\n").map((item) => {
+				const arr = item.split('"');
+				return arr[1];
+			});
 
-	// const apiResponse = await Promise.all(
-	// 	keywords.map(async (keyword) => {
-	// 		const apiResponse = await fetchAPIResponse({ keyword, length });
-	// 		const apiResponseArray = apiResponse.content.split(/\r?\n|\r|\n/g).map((item) => {
-	// 			const arr = item.split('"');
-	// 			return arr[1];
-	// 		});
+			const resObject = {};
 
-	// 		const resObject = {};
+			for (let i = 0; i < apiResponseArray.length; i++) {
+				resObject[`google_ads_${i + 1}`] = apiResponseArray[i];
+			}
 
-	// 		for (let i = 0; i < apiResponse.length; i++) {
-	// 			resObject[`google_ads_${i + 1}`] = apiResponseArray[i];
-	// 		}
+			return {
+				keyword,
+				...resObject,
+			};
+		})
+	);
 
-	// 		return {
-	// 			keyword,
-	// 			...resObject,
-	// 		};
-	// 	})
-	// );
-
-	const apiResponse = [
-		{
-			keyword: "iqos",
-			google_ads_1: "Discover IQOS - The Future of Smoking",
-			google_ads_2: "Upgrade Your Smoking Experience with IQOS",
-			google_ads_3: "Experience Smoke-Free, Tobacco Heating with IQOS",
-		},
-	];
+	// const apiResponse = [
+	// 	{
+	// 		keyword: "iqos",
+	// 		google_ads_1: "Discover IQOS - The Future of Smoking",
+	// 		google_ads_2: "Upgrade Your Smoking Experience with IQOS",
+	// 		google_ads_3: "Experience Smoke-Free, Tobacco Heating with IQOS",
+	// 	},
+	// ];
 
 	const csv = createCSV(apiResponse);
 	const csvFile = fs.writeFileSync(file, csv);
