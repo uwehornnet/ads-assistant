428e6f61d803eb404bb4bb1f650ad7a6bf5409b5
uhorn <uwehornnet@users.noreply.github.com>
2023-12-20 21:09:40 +0100

    try catch block

--- pages/api/google/ads/get.js
+++ pages/api/google/ads/get.js
@@ -27,43 +27,50 @@ const fetchAPIResponse = async ({ keyword, length }) => {
 };
 
 export default async function handler(req, res) {
-	const { params, length } = req.body;
-	const filename = "google-ads.csv";
-	const file = path.join(process.cwd(), "public/files", filename);
+	try {
+		const { params, length } = req.body;
+		const filename = "google-ads.csv";
+		const file = path.join(process.cwd(), "public/files", filename);
 
-	const keywords = params.split(",");
-	const apiResponse = await Promise.all(
-		keywords.map(async (keyword) => {
-			const apiResponse = await fetchAPIResponse({ keyword, length });
-			const apiResponseArray = apiResponse.content.split("\n").map((item) => {
-				const arr = item.split('"');
-				return arr[1];
-			});
+		const keywords = params.split(",");
+		const apiResponse = await Promise.all(
+			keywords.map(async (keyword) => {
+				const apiResponse = await fetchAPIResponse({ keyword, length });
+				const apiResponseArray = apiResponse.content.split("\n").map((item) => {
+					const arr = item.split('"');
+					return arr[1];
+				});
 
-			const resObject = {};
+				const resObject = {};
 
-			for (let i = 0; i < apiResponseArray.length; i++) {
-				resObject[`google_ads_${i + 1}`] = apiResponseArray[i];
-			}
+				for (let i = 0; i < apiResponseArray.length; i++) {
+					resObject[`google_ads_${i + 1}`] = apiResponseArray[i];
+				}
 
-			return {
-				keyword,
-				...resObject,
-			};
-		})
-	);
+				return {
+					keyword,
+					...resObject,
+				};
+			})
+		);
 
-	// const apiResponse = [
-	// 	{
-	// 		keyword: "iqos",
-	// 		google_ads_1: "Discover IQOS - The Future of Smoking",
-	// 		google_ads_2: "Upgrade Your Smoking Experience with IQOS",
-	// 		google_ads_3: "Experience Smoke-Free, Tobacco Heating with IQOS",
-	// 	},
-	// ];
+		// const apiResponse = [
+		// 	{
+		// 		keyword: "iqos",
+		// 		google_ads_1: "Discover IQOS - The Future of Smoking",
+		// 		google_ads_2: "Upgrade Your Smoking Experience with IQOS",
+		// 		google_ads_3: "Experience Smoke-Free, Tobacco Heating with IQOS",
+		// 	},
+		// ];
 
-	const csv = createCSV(apiResponse);
-	const csvFile = fs.writeFileSync(file, csv);
+		const csv = createCSV(apiResponse);
+		const csvFile = fs.writeFileSync(file, csv);
 
-	res.status(200).json({ status: "success", results: apiResponse, filepath: `/files/${filename}` });
+		res.status(200).json({ status: "success", results: apiResponse, filepath: `/files/${filename}` });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ status: "error", message: error.message });
+	}
+	
+	
 }
