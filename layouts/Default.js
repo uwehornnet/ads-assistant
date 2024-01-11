db6d37d64c3d08314b10361f998d8136b541f254
uhorn <uwehornnet@users.noreply.github.com>
2024-01-11 12:15:46 +0100

    changed kwooza Logo

--- layouts/Default.js
+++ layouts/Default.js
@@ -40,7 +40,7 @@ export default function DefaultLayout({ children }) {
 										<div className="flex-shrink-0">
 											<img
 												className="h-8 w-8"
-												src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+												src="https://www.kwooza.com/assets/img/logo.png"
 												alt="Your Company"
 											/>
 										</div>
