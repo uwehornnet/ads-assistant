33d6cf8c983c4cbc547477995b70b9ffe2da29e4
uhorn <uwehornnet@users.noreply.github.com>
2024-04-06 17:55:17 +0200

    ui changes to sing in form and home screen

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 		<DefaultLayout>
 			<main className="container mx-auto">
 				<div className="flex flex-col items-center justify-center h-screen">
-					<div className="max-w-sm w-full space-y-8">
+					<div className="max-w-sm w-full space-y-8 p-4">
 						<div>
 							<img className="mx-auto h-16 w-auto" src="/logo.png" alt="Kwooza Ads Tool" />
 							<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
@@ -24,14 +24,14 @@ export default function Home() {
 							{session ? (
 								<button
 									onClick={() => router.push("/dashboard")}
-									className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+									className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
 								>
 									Go to Dashboard
 								</button>
 							) : (
 								<button
 									onClick={() => router.push("/auth/login")}
-									className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+									className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
 								>
 									<span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
 									Sign in
